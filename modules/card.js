649fa4b5f8d505f8637f81e936eb92757168503d
ijklop90 <ijklop1234@gmail.com>
2024-03-17 11:54:55 +0300

    32

--- modules/card.js
+++ modules/card.js
@@ -1,9 +1,7 @@
-
-
 export default function renderCard(data) {
     const fragment = `
     <div class="wrapper_folder js-cell">
-    <div class="list_card ${data.category} js-card" draggable="true">
+    <div style="cursor: all-scroll" class="list_card ${data.category} js-card ${data.num}" draggable="true">
         <div class="list_card-header">${data.name}</div>
         <div class="list_card-body">${data.data}</div>
     </div>
